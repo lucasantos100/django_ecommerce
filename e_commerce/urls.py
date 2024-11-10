d21cb9719c9c1feebc37283c8bcb65a7c9830da4
Lucas Santos <lucas_santos100@estudante.sesisenai.org.br>
2024-11-10 00:56:35 -0300

    update urls.py static files and put if settingsdebug

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 
@@ -23,7 +27,10 @@ urlpatterns = [
 	path('', home_page),
 	path('about/', about_page),
 	path('contact/', contact_page),
-    path('admin/', admin.site.urls),
     path('login/', login_page),
     path('register/', register_page),
+	path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
