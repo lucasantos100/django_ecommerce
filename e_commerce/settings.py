17066084ad5f62461121d5bab07be3faf5417b8c
Lucas Santos <lucas_santos100@estudante.sesisenai.org.br>
2024-11-11 23:50:10 -0300

    update nos settings do ecommerce pra ele receber o app product, e update na pasta admin pra receber o products tambem

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -32,12 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #app do lucas django
+    'products'
 ]
 
 MIDDLEWARE = [
