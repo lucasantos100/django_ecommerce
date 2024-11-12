ff9d40e75a55d611c04e9538ba5b9f3acbdc77ba
Lucas Santos <lucas_santos100@estudante.sesisenai.org.br>
2024-11-12 16:22:38 -0300

    criacao da paste templates/products/list.html em products e modificacao do template_name na cbv em views.py

--- products/views.py
+++ products/views.py
@@ -4,6 +4,17 @@ from django.views.generic import ListView
 from .models import Product
 
 # Create your views here.
+
+#cbv
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+#fbv
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'qs': queryset
+    }
+    return render(request, "products/list.html", context)   
