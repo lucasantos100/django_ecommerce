26243dd723198a6e2fec0533aa83da3afd08039f
Lucas Santos <lucas_santos100@estudante.sesisenai.org.br>
2024-11-12 16:08:52 -0300

    criacao da class bv e function bv no products/views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Product
 
 # Create your views here.
+class ProductListView(ListView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
